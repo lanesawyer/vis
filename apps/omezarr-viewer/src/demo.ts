ffc26a04bdea7faa26212554fdbc25cc51405fb1
noah <noah.shepard@alleninstitute.org>
2024-01-31 13:38:49 -0800

    add a bunch of controls to the demo, remove experimental zarr loader libs, fix math in shader

--- apps/omezarr-viewer/src/demo.ts
+++ apps/omezarr-viewer/src/demo.ts
@@ -12,7 +12,8 @@ import {
   getVisibleTiles,
   requestsForTile,
 } from "./slice-renderer";
-import { Box2D, box2D } from "@aibs-vis/geometry";
+import { Box2D, Interval, Vec2, box2D, vec2 } from "@aibs-vis/geometry";
+import { FrameLifecycle } from "@aibs-vis/scatterbrain/lib/render-queue";
 
 const file =
   "https://aind-open-data.s3.amazonaws.com/SmartSPIM_644106_2022-12-09_12-12-39_stitched_2022-12-16_16-55-11/processed/OMEZarr/Ex_488_Em_525.zarr";
@@ -22,15 +23,17 @@ function renderAFrame(
   cache: AsyncDataCache<REGL.Texture2D>,
   view: box2D,
   plane: AxisAlignedPlane,
+  planeIndex: number,
   dataset: ZarrDataset,
   viewport: REGL.BoundingBox,
+  gamut: Interval,
   renderer: (
     item: VoxelTile,
     settings: VoxelSliceRenderSettings,
     tasks: Record<string, REGL.Texture2D | undefined>
   ) => void
 ) {
-  const visibleTiles = getVisibleTiles(view, plane, dataset);
+  const visibleTiles = getVisibleTiles(view, plane, planeIndex, dataset);
   const frame = beginLongRunningFrame<REGL.Texture2D, VoxelTile, VoxelSliceRenderSettings>(
     5,
     33,
@@ -41,6 +44,7 @@ function renderAFrame(
       dataset,
       regl,
       viewport,
+      gamut,
     },
     requestsForTile,
     renderer,
@@ -62,6 +66,83 @@ function renderAFrame(
     },
     cacheKeyFactory
   );
+  return frame;
+}
+
+class Demo {
+  mouse: "up" | "down";
+  mousePos: vec2;
+  view: box2D;
+  gamut: Interval;
+  plane: AxisAlignedPlane;
+  sliceIndex: number;
+  canvas: HTMLCanvasElement;
+  curFrame: FrameLifecycle | null;
+  onChange: (state: Demo) => FrameLifecycle;
+  constructor(canvas: HTMLCanvasElement, size: vec2, change: (state: Demo) => FrameLifecycle) {
+    this.sliceIndex = 50;
+    this.plane = "xy";
+    this.mouse = "up";
+    this.view = Box2D.create([0, 0], size);
+    this.mousePos = [0, 0];
+    this.gamut = { min: 0, max: 2500 };
+    this.onChange = change;
+    this.canvas = canvas;
+    this.curFrame = null;
+  }
+  rerender() {
+    if (this.curFrame !== null) {
+      this.curFrame.cancelFrame();
+    }
+    this.curFrame = this.onChange(this);
+  }
+  mouseButton(click: "up" | "down") {
+    this.mouse = click;
+  }
+  mouseMove(delta: vec2) {
+    if (this.mouse === "down") {
+      // drag the view
+      this.view = Box2D.translate(this.view, delta);
+      this.rerender();
+    }
+    this.mousePos = Vec2.add(this.mousePos, delta);
+  }
+  changeSlice(delta: number) {
+    this.sliceIndex += delta;
+    this.rerender();
+  }
+  changeGamut(delta: number) {
+    this.gamut = { min: this.gamut.min, max: this.gamut.max + delta };
+    this.rerender();
+  }
+  zoom(scale: number) {
+    const m = Box2D.midpoint(this.view);
+    this.view = Box2D.translate(Box2D.scale(Box2D.translate(this.view, Vec2.scale(m, -1)), [scale, scale]), m);
+    this.rerender();
+  }
+}
+
+function setupEventHandlers(canvas: HTMLCanvasElement, demo: Demo) {
+  canvas.onmousedown = (e: MouseEvent) => {
+    demo.mouseButton("down");
+  };
+  canvas.onmouseup = (e: MouseEvent) => {
+    demo.mouseButton("up");
+  };
+  canvas.onmousemove = (e: MouseEvent) => {
+    // account for gl-origin vs. screen origin:
+    demo.mouseMove([-e.movementX, e.movementY]);
+  };
+  canvas.onwheel = (e: WheelEvent) => {
+    if (e.ctrlKey || e.altKey) {
+      demo.changeSlice(e.deltaY > 0 ? 1 : -1);
+    } else if (e.shiftKey) {
+      demo.changeGamut(e.deltaY);
+    } else {
+      demo.zoom(e.deltaY > 0 ? 1.1 : 0.9);
+    }
+  };
+  // canvas.onkeyup = (e: KeyboardEvent) => {};
 }
 async function demotime() {
   const regl = REGL({
@@ -73,26 +154,42 @@ async function demotime() {
     },
     extensions: ["ANGLE_instanced_arrays", "OES_texture_float", "WEBGL_color_buffer_float"],
   });
-  const voxelSliceCache: AsyncDataCache<REGL.Texture2D> = new AsyncDataCache<REGL.Texture2D>();
   const canvas: HTMLCanvasElement = regl._gl.canvas as HTMLCanvasElement;
-  const view = Box2D.create([0, 0], [512, 512]);
+
+  const viewport = {
+    x: 0,
+    y: 0,
+    width: canvas.clientWidth,
+    height: canvas.clientHeight,
+  };
+  const voxelSliceCache: AsyncDataCache<REGL.Texture2D> = new AsyncDataCache<REGL.Texture2D>();
+  const imageRenderer = buildImageRenderer(regl);
   const zarr = await load(file);
   regl.clear({ color: [0, 0, 0, 1], depth: 1 });
-  const imageRenderer = buildImageRenderer(regl);
-  renderAFrame(
-    regl,
-    voxelSliceCache,
-    view,
-    "xy",
-    zarr,
-    {
+
+  const renderPlease = (demo: Demo) => {
+    const viewport = {
       x: 0,
       y: 0,
-      width: canvas.clientWidth,
-      height: canvas.clientHeight,
-    },
-    imageRenderer
-  );
+      width: demo.canvas.width,
+      height: demo.canvas.height,
+    };
+    regl.clear({ color: [0, 0, 0, 1], depth: 1 });
+    return renderAFrame(
+      regl,
+      voxelSliceCache,
+      demo.view,
+      demo.plane,
+      demo.sliceIndex,
+      zarr,
+      viewport,
+      demo.gamut,
+      imageRenderer
+    );
+  };
+  const theDemo = new Demo(canvas, [viewport.width, viewport.height], renderPlease);
+  setupEventHandlers(canvas, theDemo);
+  theDemo.rerender();
 }
 
 // since I am just included in a script tag in a raw html document, this is how we start:
--- apps/omezarr-viewer/src/slice-renderer.ts
+++ apps/omezarr-viewer/src/slice-renderer.ts
@@ -1,6 +1,6 @@
 import REGL from "regl";
 import { ZarrDataset, ZarrRequest, getSlice, pickBestScale, sizeInVoxels } from "./zarr-data";
-import { Box2D, Vec2, box2D, vec2, vec4 } from "@aibs-vis/geometry";
+import { Box2D, Interval, Vec2, box2D, vec2, vec4 } from "@aibs-vis/geometry";
 import { omit } from "lodash";
 
 type Props = { tile: vec4; view: vec4; gamut: vec2; viewport: REGL.BoundingBox; img: REGL.Texture2D };
@@ -15,7 +15,8 @@ export function buildImageRenderer(regl: REGL.Regl) {
         void main(){
            vec2 tileSize = tile.zw-tile.xy;
            texCoord = pos;
-            vec2 p = (pos.xy*tileSize+tile.xy)/(view.zw-view.xy);
+           vec2 obj = (pos.xy*tileSize+tile.xy);
+            vec2 p = (obj-view.xy)/(view.zw-view.xy);
             // now, to clip space
             p = (p*2.0)-1.0;
             gl_Position = vec4(p.x,p.y,0.0,1.0);
@@ -40,21 +41,21 @@ export function buildImageRenderer(regl: REGL.Regl) {
       gamut: regl.prop<Props, "gamut">("gamut"),
     },
     count: 4,
-    viewport: regl.prop<Props, "viewport">("viewport"),
+    // viewport: regl.prop<Props, "viewport">("viewport"),
     primitive: "triangle fan",
     // ... more!
   });
 
   return (item: VoxelTile, settings: VoxelSliceRenderSettings, tasks: Record<string, Bfr | undefined>) => {
-    const { view, viewport } = settings;
+    const { view, viewport, gamut } = settings;
     const { bounds } = item;
     const img = tasks[LUMINANCE];
     if (!img) return; // we cant render if the data for the positions is missing!
     cmd({
       view: [...view.minCorner, ...view.maxCorner],
       tile: [...bounds.minCorner, ...bounds.maxCorner],
-      viewport,
-      gamut: [0, 2500],
+      // viewport,
+      gamut: [gamut.min, gamut.max],
       img,
     });
   };
@@ -66,6 +67,7 @@ export type VoxelSliceRenderSettings = {
   regl: REGL.Regl;
   dataset: ZarrDataset;
   view: box2D;
+  gamut: Interval;
   viewport: REGL.BoundingBox;
 };
 export type AxisAlignedPlane = "xy" | "yz" | "xz";
@@ -151,7 +153,12 @@ const uvTable = {
   yz: { u: "y", v: "z" },
 } as const;
 
-export function getVisibleTiles(view: box2D, plane: AxisAlignedPlane, dataset: ZarrDataset): VoxelTile[] {
+export function getVisibleTiles(
+  view: box2D,
+  plane: AxisAlignedPlane,
+  planeIndex: number,
+  dataset: ZarrDataset
+): VoxelTile[] {
   const thingy = pickBestScale(dataset);
   const size = sizeInVoxels(uvTable[plane], dataset.multiscales[0].axes, thingy);
   if (!size) return [];
@@ -161,6 +168,6 @@ export function getVisibleTiles(view: box2D, plane: AxisAlignedPlane, dataset: Z
     .map((uv) => ({
       plane,
       bounds: uv,
-      planeIndex: 50,
+      planeIndex,
     }));
 }

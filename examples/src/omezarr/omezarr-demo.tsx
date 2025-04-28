e0adbc28a7d93aedad931e649367d0e31eeaac6e
Lane Sawyer <github@lanesawyer.dev>
2025-04-28 09:40:56 -0700

    oops types!

--- examples/src/omezarr/omezarr-demo.tsx
+++ examples/src/omezarr/omezarr-demo.tsx
@@ -1,4 +1,4 @@
-import { Box2D, type Interval, PLANE_XY, type box2D, type vec2 } from '@alleninstitute/vis-geometry';
+import { Box2D, type Interval, PLANE_XY, Vec2, type box2D, type vec2 } from '@alleninstitute/vis-geometry';
 import { type OmeZarrMetadata, loadMetadata, sizeInUnits } from '@alleninstitute/vis-omezarr';
 import type { RenderSettings, RenderSettingsChannels } from '@alleninstitute/vis-omezarr';
 import { logger, type WebResource } from '@alleninstitute/vis-core';

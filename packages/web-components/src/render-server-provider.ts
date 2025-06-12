d059377a3258bf15cc2326feb76a2c55272e989b
Lane Sawyer <lane.sawyer@alleninstitute.org>
2025-06-11 21:50:20 -0700

    pass render server features by attribute

--- packages/web-components/src/render-server-provider.ts
+++ packages/web-components/src/render-server-provider.ts
@@ -4,24 +4,35 @@ import { REQUEST_RENDER_SERVER } from './base-viewer';
 export const RENDER_SERVER_TAG_NAME = 'render-server-provider';
 export const RENDER_SERVER_READY = 'render-server-ready';
 
+export const DEFAULT_MAX_SIZE = 4096;
+
 export class RenderServerProvider extends HTMLElement {
     private renderServer: RenderServer;
+    // TODO: Change to warn once I'm done working on the viewer components
     private logger = new Logger(RENDER_SERVER_TAG_NAME, 'info');
 
     constructor() {
         super();
         this.logger.info('Created');
-        // TODO: Pass in render server options
-        // Initialize RenderServer with a reasonable max size and oes_texture_float extension for OME-Zarr viewer
-        this.renderServer = new RenderServer([4096, 4096], ['oes_texture_float']);
+
+        const oes_texture_float = this.getAttribute('oes_texture_float');
+        const extensions = oes_texture_float ? ['oes_texture_float'] : [];
+
+        this.logger.info(`Using extensions: ${extensions.join(', ')}`);
+
+        this.renderServer = new RenderServer([DEFAULT_MAX_SIZE, DEFAULT_MAX_SIZE], extensions);
+    }
+
+    static get observedAttributes() {
+        return ['oes_texture_float'];
     }
 
     connectedCallback() {
         this.logger.info('Connected');
         // Listen for requests from child components
         this.addEventListener(REQUEST_RENDER_SERVER, (event: Event) => {
-            this.logger.info('Received request for render server');
             event.stopPropagation();
+            this.logger.info('Received request for render server');
             const targetEl = event.target as HTMLElement;
             const responseEvent = new CustomEvent(RENDER_SERVER_READY, {
                 detail: this.renderServer,
